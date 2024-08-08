de38f883a35d8f76ed783639e678c1938b2b69d2
Muyiwa Obaremi <obaremimuyiwa@gmail.com>
2024-08-08 11:26:35 +0100

    routes created

---
+++ pkg/routes/bookstore-routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/Muyi2905/GoLibManager/pkg/controllers"
+)
+
+var RegisterBookstoreRoutes = func (router *mux.Router)  {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{id}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{id}", controllers.UpdateById).Methods("PUT")
+	router.HandleFunc("/book/{id}", controllers.DeleteByID).Methods("DELETE")
+}
+
