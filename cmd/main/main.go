ef2503163624b459170714fbbdc1554de478c7ca
Muyiwa Obaremi <obaremimuyiwa@gmail.com>
2024-08-12 13:56:14 +0100

    server setup

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
-
-	"github.com/gorilla/mux"
-	"net/http"
-	"github.com/github.com/Muyi2905/GoLibManager/pkg/routes"
 	"log"
+	"net/http"
 
+	"github.com/Muyi2905/GoLibManager/pkg/routes"
+	"github.com/github.com/Muyi2905/GoLibManager/pkg/routes"
+	"github.com/gorilla/mux"
 )
 
+func main(){
+	r:= mux.NewRouter()
+	routes.RegisterBookstoreRoutes(r)
+http.Handle("/", r)
+log.Fatal(http.ListenAndServe(":8080", r))	
+}
