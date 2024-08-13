c4a93a79407e1cdf570ad5d0044a87ab30838abc
Muyiwa Obaremi <obaremimuyiwa@gmail.com>
2024-08-13 15:20:58 +0100

    update

--- pkg/controllers/book-controllers.go
+++ pkg/controllers/book-controllers.go
@@ -40,3 +40,9 @@ func GetBookById(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
+
+func CreateBook(w http.ResponseWriter, r*http.Request){
+
+	book:= models.Book()
+
+}
--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -6,12 +6,12 @@ import (
 	"net/http"
 )
 
-func ParseForm (r *http.Request, x interface{}){
-	if body, err := ioutil.ReadAll(r.Body); err!= nil{
-		if err:= json.Unmarshal([]byte(body),x); err!= nil{
-			return
 
-		}
-	}
-	
+
+func ParseForm(r *http.Request, x interface{}) {
+    if body, err := ioutil.ReadAll(r.Body); err != nil {
+        if err := json.Unmarshal([]byte(body), x); err != nil {
+            return
+        }
+    }
 }
