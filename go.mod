20a2f524c357223c223ab78aa3b66cdcbcd7471c
Muyiwa Obaremi <obaremimuyiwa@gmail.com>
2024-08-15 10:47:17 +0100

    env

--- go.mod
+++ go.mod
@@ -3,8 +3,12 @@ module github.com/Muyi2905/GoLibManager
 go 1.22.4
 
 require (
+	github.com/gorilla/mux v1.8.1
+	github.com/jinzhu/gorm v1.9.16
+)
+
+require (
 	github.com/go-sql-driver/mysql v1.5.0 // indirect
-	github.com/gorilla/mux v1.8.1 // indirect
-	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 )
--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,23 +1,40 @@
 package config
 
 import (
-    "github.com/jinzhu/gorm"
-    _ "github.com/jinzhu/gorm/dialects/mysql" // Required for MySQL dialect
-    "log"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql" // MySQL driver
+	"github.com/joho/godotenv"
+	"log"
+	"os"
 )
 
 var db *gorm.DB
 
 func Connect() {
-    var err error
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
 
-    d, err := gorm.Open("mysql", "muyiwa:Muyi2905@tcp(127.0.0.1:3306)/golang?charset=utf8&parseTime=True&loc=Local")
-    if err != nil {
-        log.Fatalf("Could not connect to the database: %v", err)
-    }
-    db = d
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+	dbCharset := os.Getenv("DB_CHARSET")
+	dbParseTime := os.Getenv("DB_PARSE_TIME")
+	dbLoc := os.Getenv("DB_LOC")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=%s&loc=%s",
+		dbUser, dbPassword, dbHost, dbPort, dbName, dbCharset, dbParseTime, dbLoc)
+
+	db, err = gorm.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalf("Could not connect to the database: %v", err)
+	}
 }
 
 func GetDB() *gorm.DB {
-    return db
+	return db
 }
