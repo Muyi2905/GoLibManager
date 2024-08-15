a1b54582a699ee8dd597a99af819afd21b3b4603
Muyiwa Obaremi <obaremimuyiwa@gmail.com>
2024-08-15 10:13:37 +0100

    fix databasse conncetion

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,20 +1,23 @@
 package config
 
 import (
-	"github.com/jinzhu/gorm"
+    "github.com/jinzhu/gorm"
+    _ "github.com/jinzhu/gorm/dialects/mysql" // Required for MySQL dialect
+    "log"
 )
 
 var db *gorm.DB
 
 func Connect() {
-	d, err := gorm.Open("mysql", "muyiwa:Muyi2905@/golang?charset=utf8&parseTime=True&loc=Local")
-	if err != nil {
-		panic(err)
-	}
-	db = d
+    var err error
+
+    d, err := gorm.Open("mysql", "muyiwa:Muyi2905@tcp(127.0.0.1:3306)/golang?charset=utf8&parseTime=True&loc=Local")
+    if err != nil {
+        log.Fatalf("Could not connect to the database: %v", err)
+    }
+    db = d
 }
 
 func GetDB() *gorm.DB {
-	return db
-
+    return db
 }
