37c5dd5d43b50fee491b0d40a12262fe8091d9ae
Muyiwa Obaremi <obaremimuyiwa@gmail.com>
2024-08-15 09:59:03 +0100

    cleaned app.go

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -2,20 +2,19 @@ package config
 
 import (
 	"github.com/jinzhu/gorm"
-	
 )
 
 var db *gorm.DB
 
-func Connect(){
+func Connect() {
 	d, err := gorm.Open("mysql", "muyiwa:Muyi2905@/golang?charset=utf8&parseTime=True&loc=Local")
-		if err!= nil{
-			panic(err)
-		}
-db = d	
+	if err != nil {
+		panic(err)
+	}
+	db = d
 }
 
-func GetDB () *gorm.DB{
+func GetDB() *gorm.DB {
 	return db
-	
+
 }
